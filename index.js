10235a6ee61c2a23441cbdfd2a3172b11bc24070
Troy <troyakins99@yahoo.com>
2021-07-02 16:20:29 -0500

    commit tree 3d6b90606e4dad474f9bf2a469a6f946426b7ed2

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+//why won't this pass the test, but it works! 
+
+// function upperCaseCustomerName(){
+//     let upper = customerName.toUpperCase(); return upper;
+// }
+
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = "test";
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "test again"
+}
+//console.log(overwriteBestCustomer())
+    
